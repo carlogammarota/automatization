892de0bcb57afc1eb988a618a398424a4fd45b1c
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-10-09 22:12:28 -0300

    update

--- index.js
+++ index.js
@@ -41,68 +41,70 @@ async function clonarArchivoDominioDefault(subdomain, port) {
   }
 }
 
-async function recargarNginx() {
-  const comando = "sudo systemctl reload nginx";
 
-  try {
-    const { stdout, stderr } = await exec(comando);
-    console.log(`Resultado: ${stdout}`);
-    console.error(`Errores: ${stderr}`);
-  } catch (error) {
-    console.error(`Error al recargar Nginx: ${error}`);
-    throw new Error("Error al recargar Nginx");
-  }
-}
-
-async function crearSubdominioCloudFlare(subdomain) {
-  const zoneId = "22ba6192a10c766dd77527c7a101ad35";
-  const apiKey = "9ed98c1d2991f51503bd165e5d61924cae9d4";
-  const authEmail = "carlo.gammarota@gmail.com";
-  const dnsRecordData = {
-    type: "A",
-    name: subdomain,
-    content: "64.227.76.217",
-    ttl: 1,
-    proxied: true,
-  };
-
-  const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
-
-  const config = {
-    method: "post",
-    url: apiUrl,
-    headers: {
-      "X-Auth-Key": apiKey,
-      "X-Auth-Email": authEmail,
-      "Content-Type": "application/json",
-    },
-    data: dnsRecordData,
-  };
-
-  try {
-    const response = await axios(config);
-    console.log("Registro DNS agregado con éxito:", response.data);
-    return response.data;
-
-  } catch (error) {
-    console.error("Error al agregar el registro DNS:", error.response.data);
-    throw new Error("Error al agregar el registro DNS (CloudFlare)", error);
-  }
-}
 
 app.post("/build-and-create", async (req, res) => {
+    async function recargarNginx() {
+        const comando = "sudo systemctl reload nginx";
+      
+        try {
+          const { stdout, stderr } = await exec(comando);
+          console.log(`Resultado: ${stdout}`);
+          console.error(`Errores: ${stderr}`);
+        } catch (error) {
+          console.error(`Error al recargar Nginx: ${error}`);
+          throw new Error("Error al recargar Nginx");
+        }
+      }
+      
+      async function crearSubdominioCloudFlare(subdomain) {
+        const zoneId = "22ba6192a10c766dd77527c7a101ad35";
+        const apiKey = "9ed98c1d2991f51503bd165e5d61924cae9d4";
+        const authEmail = "carlo.gammarota@gmail.com";
+        const dnsRecordData = {
+          type: "A",
+          name: subdomain,
+          content: "64.227.76.217",
+          ttl: 1,
+          proxied: true,
+        };
+      
+        const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
+      
+        const config = {
+          method: "post",
+          url: apiUrl,
+          headers: {
+            "X-Auth-Key": apiKey,
+            "X-Auth-Email": authEmail,
+            "Content-Type": "application/json",
+          },
+          data: dnsRecordData,
+        };
+      
+        try {
+          const response = await axios(config);
+          console.log("Registro DNS agregado con éxito:", response.data);
+          return response.data;
+      
+        } catch (error) {
+          console.error("Error al agregar el registro DNS:", error.response.data);
+          throw new Error("Error al agregar el registro DNS (CloudFlare)", error);
+        }
+      }
   let { hostPort, subdomain } = req.body;
   const containerName = subdomain;
   const buildCommand = `docker run -d --name ${containerName} -p ${hostPort}:2222 mi-app:latest`;
 
   try {
-    let data = await crearSubdominioCloudFlare(subdomain);
+    await crearSubdominioCloudFlare(subdomain);
+
     await clonarArchivoDominioDefault(subdomain, hostPort);
     await exec(buildCommand);
 
-    console.log("Imagen Docker construida con éxito", data);
+    console.log("Imagen Docker construida con éxito");
     await recargarNginx();
-    res.send("Imagen Docker construida con éxito", data);
+    res.send("Imagen Docker construida con éxito");
   } catch (error) {
     console.error("Error general:", error);
     res.status(500).send("Error general: " + error.message);
