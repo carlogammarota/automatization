718a0d2da57fa4f1120d5dd64514932965f6f440
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-10-09 21:20:06 -0300

    update

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const app = express();
 const port = 2323;
 const path = require("path");
 const fs = require("fs");
+const axios = require("axios");
 // const crearSubdominio = require('./crearSubdominio');
 
 app.use(express.json());
 app.use(bodyParser.json());
 
-function clonarArchivoDefault(subdomain, port) {
+function clonarArchivoDominioDefault(subdomain, port) {
   const archivoDefault = "default.conf"; // Nombre del archivo default
   const nuevoNombre = `${subdomain}.armortemplate.site`; // Nombre del archivo clonado
   const rutaDestino = `/etc/nginx/sites-enabled/${nuevoNombre}`; // Ruta de destino
@@ -32,6 +33,7 @@ function clonarArchivoDefault(subdomain, port) {
     fs.writeFile(nuevoNombre, nuevoContenido, "utf8", (error) => {
       if (error) {
         console.error(`Error al escribir el archivo ${nuevoNombre}: ${error}`);
+        return res.status(500).send("Error al escribir el archivo");
       } else {
         console.log(`Archivo ${nuevoNombre} creado con éxito.`);
 
@@ -40,9 +42,11 @@ function clonarArchivoDefault(subdomain, port) {
           `sudo mv ${nuevoNombre} ${rutaDestino}`,
           (error, stdout, stderr) => {
             if (error) {
+              
               console.error(
                 `Error al mover el archivo a ${rutaDestino}: ${error}`
               );
+              return res.status(500).send("Error al mover el archivo");
             } else {
               console.log(`Archivo movido a ${rutaDestino} con éxito.`);
             }
@@ -59,7 +63,7 @@ function recargarNginx() {
   exec(comando, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error al ejecutar el comando: ${error}`);
-      return;
+      return res.status(500).send("Error al hacer reload nginx");
     }
 
     console.log(`Resultado: ${stdout}`);
@@ -67,15 +71,53 @@ function recargarNginx() {
   });
 }
 
+function crearSubdominio(subdomain, port) {
+  const zoneId = "22ba6192a10c766dd77527c7a101ad35"; // Reemplaza con tu ID de zona
+  const apiKey = "9ed98c1d2991f51503bd165e5d61924cae9d4";
+  const authEmail = "carlo.gammarota@gmail.com";
+
+  // Datos del registro DNS que deseas agregar
+  const dnsRecordData = {
+    type: "A", // Tipo de registro DNS (por ejemplo, A, CNAME, MX, etc.)
+    name: subdomain, // Nombre del subdominio
+    content: "64.227.76.217", // Contenido del registro (por ejemplo, una dirección IP)
+    ttl: 1, // Tiempo de vida en segundos
+    proxied: true, // Si deseas que la solicitud se enruté a través de Cloudflare (true/false)
+  };
+
+  const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
+
+  // Configuración de la solicitud
+  const config = {
+    method: "post", // Utiliza el método POST para crear el registro DNS
+    url: apiUrl,
+    headers: {
+      "X-Auth-Key": apiKey,
+      "X-Auth-Email": authEmail,
+      "Content-Type": "application/json",
+    },
+    data: dnsRecordData,
+  };
+
+  // Realizar la solicitud POST para agregar el registro DNS
+  axios(config)
+    .then((response) => {
+      console.log("Registro DNS agregado con éxito:", response.data);
+      
+    })
+    .catch((error) => {
+      
+      console.error(
+        "Error al agregar el registro DNS:",
+        error.response.data
+      );
+      return res.status(500).send("Error al agregar el registro DNS (CloudFlare)");
+    });
+}
+
 app.post("/build-and-create", (req, res) => {
   let { hostPort, containerName, subdomain } = req.body;
-  console.log(req.body);
 
-  // Definir las variables necesarias
-  const imageName = "mi-app:latest"; // Reemplaza con el nombre de tu imagen
-  // const exposedPort = '1313'; // Reemplaza con el puerto que deseas exponer
-
-  // Comando para construir la imagen Docker desde el Dockerfile
   const buildCommand = `docker run -d --name ${containerName} -p ${hostPort}:2222 mi-app:latest`;
 
   exec(buildCommand, (error, stdout, stderr) => {
@@ -85,92 +127,16 @@ app.post("/build-and-create", (req, res) => {
     } else {
       console.log("Imagen Docker construida con éxito");
 
-      //aca hay que crear el subdominio
-      // Ejemplo de uso:
-      clonarArchivoDefault(subdomain, hostPort);
-      const axios = require("axios");
-
-      const zoneId = "22ba6192a10c766dd77527c7a101ad35"; // Reemplaza con tu ID de zona
-      const apiKey = "9ed98c1d2991f51503bd165e5d61924cae9d4";
-      const authEmail = "carlo.gammarota@gmail.com";
-
-      // Datos del registro DNS que deseas agregar
-      const dnsRecordData = {
-        type: "A", // Tipo de registro DNS (por ejemplo, A, CNAME, MX, etc.)
-        name: subdomain, // Nombre del subdominio
-        content: "64.227.76.217", // Contenido del registro (por ejemplo, una dirección IP)
-        ttl: 1, // Tiempo de vida en segundos
-        proxied: true, // Si deseas que la solicitud se enruté a través de Cloudflare (true/false)
-      };
-
-      const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
-
-      // Configuración de la solicitud
-      const config = {
-        method: "post", // Utiliza el método POST para crear el registro DNS
-        url: apiUrl,
-        headers: {
-          "X-Auth-Key": apiKey,
-          "X-Auth-Email": authEmail,
-          "Content-Type": "application/json",
-        },
-        data: dnsRecordData,
-      };
-
-      // Realizar la solicitud POST para agregar el registro DNS
-      axios(config)
-        .then((response) => {
-          console.log("Registro DNS agregado con éxito:", response.data);
-        })
-        .catch((error) => {
-          console.error(
-            "Error al agregar el registro DNS:",
-            error.response.data
-          );
-        });
+      //creamos subdominio
+      clonarArchivoDominioDefault(subdomain, hostPort);
+      crearSubdominio(subdomain, hostPort);
 
       // Llama a la función para recargar Nginx
       recargarNginx();
       return res.send("Imagen Docker construida con éxito");
-
-      // Una vez construida la imagen, crear y ejecutar el contenedor
-      const docker = new Docker({ socketPath: "/var/run/docker.sock" });
-
-      const containerConfig = {
-        Image: imageName,
-        name: containerName,
-        ExposedPorts: {
-          [`${exposedPort}/tcp`]: {}, // Exponer el puerto específico
-        },
-        HostConfig: {
-          PortBindings: {
-            [`${exposedPort}/tcp`]: [{ HostPort: hostPort }], // Mapeo del puerto en el host
-          },
-        },
-      };
-
-      // docker.createContainer(containerConfig, (createError, container) => {
-      //   if (createError) {
-      //     console.error('Error al crear el contenedor Docker:', createError);
-      //     res.status(500).send('Error al crear el contenedor Docker');
-      //   } else {
-      //     container.start((startError) => {
-      //       if (startError) {
-      //         console.error('Error al iniciar el contenedor Docker:', startError);
-      //         res.status(500).send('Error al iniciar el contenedor Docker');
-      //       } else {
-      //         console.log('Contenedor Docker iniciado con éxito');
-      //         res.send('Contenedor Docker iniciado con éxito');
-      //       }
-      //     });
-      //   }
-      // });
     }
   });
 });
-
-// Ejemplo de uso:
-// clonarArchivoDefault('empresa', '8989');
 
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
