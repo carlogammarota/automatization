5a33f60a91e95b1ea845c4ea854c7ff37ee15f9c
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-10-09 22:39:45 -0300

    update

--- index.js
+++ index.js
@@ -13,29 +13,27 @@ app.use(bodyParser.json());
 
 // Middleware para manejar errores
 app.use((err, req, res, next) => {
-    console.error(err); // Imprime el error en la consola
-    res.status(500).send('Error interno del servidor'); // Responde con un mensaje de error
-  });
+  console.error(err); // Imprime el error en la consola
+  res.status(500).send('Error interno del servidor'); // Responde con un mensaje de error
+});
 
 // Contraseña maestra
-const masterPassword = 'contraseña_maestra';    
+const masterPassword = 'contraseña_maestra';
 
 // Middleware personalizado para verificar la contraseña maestra
 function checkMasterPassword(req, res, next) {
-    const password = req.body.password;
-  
-    if (password === masterPassword) {
-      // La contraseña es correcta, pasa al siguiente middleware
-      next();
-    } else {
-      // La contraseña es incorrecta, devuelve un error 401 (No autorizado)
-      
-      res.status(401).json({ message: 'Contraseña incorrecta' });
-    }
-  }
+  const password = req.body.password;
 
-  
-async function clonarArchivoDominioDefault(subdomain, port) {
+  if (password === masterPassword) {
+    // La contraseña es correcta, pasa al siguiente middleware
+    next();
+  } else {
+    // La contraseña es incorrecta, devuelve un error 401 (No autorizado)
+    res.status(401).json({ message: 'Contraseña incorrecta' });
+  }
+}
+
+async function clonarArchivoDominioDefault(subdomain, port, res) {
   const archivoDefault = "default.conf";
   const nuevoNombre = `${subdomain}.armortemplate.site`;
   const rutaDestino = `/etc/nginx/sites-enabled/${nuevoNombre}`;
@@ -53,86 +51,73 @@ async function clonarArchivoDominioDefault(subdomain, port) {
     console.log(`Archivo movido a ${rutaDestino} con éxito.`);
   } catch (error) {
     console.error(`Error al clonar el archivo: ${error}`);
-    throw new Error("Error al clonar el archivo");
+    res.status(500).send('Error al clonar el archivo');
   }
 }
-async function recargarNginx() {
-    const comando = "sudo systemctl reload nginx";
 
-    try {
-      const { stdout, stderr } = await exec(comando);
-      console.log(`Resultado: ${stdout}`);
-      console.error(`Errores: ${stderr}`);
-    } catch (error) {
-      console.error(`Error al recargar Nginx: ${error}`);
-      throw new Error("Error al recargar Nginx:", error);
-      //throw new Error("Error al recargar Nginx");
-    }
+async function recargarNginx(res) {
+  const comando = "sudo systemctl reload nginx";
+
+  try {
+    const { stdout, stderr } = await exec(comando);
+    console.log(`Resultado: ${stdout}`);
+    console.error(`Errores: ${stderr}`);
+  } catch (error) {
+    console.error(`Error al recargar Nginx: ${error}`);
+    res.status(500).send('Error al recargar Nginx');
   }
-  async function crearSubdominioCloudFlare(subdomain) {
-    const zoneId = "22ba6192a10c766dd77527c7a101ad35";
-    const apiKey = "9ed98c1d2991f51503bd165e5d61924cae9d4";
-    const authEmail = "carlo.gammarota@gmail.com";
-    const dnsRecordData = {
-      type: "A",
-      name: subdomain,
-      content: "64.227.76.217",
-      ttl: 1,
-      proxied: true,
-    };
+}
 
-    const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
+async function crearSubdominioCloudFlare(subdomain, res) {
+  const zoneId = "22ba6192a10c766dd77527c7a101ad35";
+  const apiKey = "9ed98c1d2991f51503bd165e5d61924cae9d4";
+  const authEmail = "carlo.gammarota@gmail.com";
+  const dnsRecordData = {
+    type: "A",
+    name: subdomain,
+    content: "64.227.76.217",
+    ttl: 1,
+    proxied: true,
+  };
 
-    const config = {
-      method: "post",
-      url: apiUrl,
-      headers: {
-        "X-Auth-Key": apiKey,
-        "X-Auth-Email": authEmail,
-        "Content-Type": "application/json",
-      },
-      data: dnsRecordData,
-    };
+  const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
 
-    try {
-      const response = await axios(config);
-      console.log("Registro DNS agregado con éxito:", response.data);
-      return response.data;
-    } catch (error) {
-      console.error("Error al agregar el registro DNS:", error);
-    //   return
-    //mostrar error pero no detener el proceso
+  const config = {
+    method: "post",
+    url: apiUrl,
+    headers: {
+      "X-Auth-Key": apiKey,
+      "X-Auth-Email": authEmail,
+      "Content-Type": "application/json",
+    },
+    data: dnsRecordData,
+  };
 
-
-    //   return res.status(500).send({
-    //     message: "Error al agregar el registro DNS (CloudFlare)",
-    //     error: error,
-    //   });
-        throw new Error("Error al agregar el registro DNS (CloudFlare)");
-    }
+  try {
+    const response = await axios(config);
+    console.log("Registro DNS agregado con éxito:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error al agregar el registro DNS:", error);
+    res.status(500).send('Error al agregar el registro DNS (CloudFlare)');
   }
-
+}
 
 app.post("/build-and-create", checkMasterPassword, async (req, res) => {
-
   let { hostPort, subdomain } = req.body.data;
-  let { password } = req.body;
   const containerName = subdomain;
   const buildCommand = `docker run -d --name ${containerName} -p ${hostPort}:2222 mi-app:latest`;
 
   try {
-    await crearSubdominioCloudFlare(subdomain);
-
-    await clonarArchivoDominioDefault(subdomain, hostPort);
+    await crearSubdominioCloudFlare(subdomain, res);
+    await clonarArchivoDominioDefault(subdomain, hostPort, res);
     await exec(buildCommand);
-
     console.log("Imagen Docker construida con éxito");
-    await recargarNginx();
+    await recargarNginx(res);
     res.send("Imagen Docker construida con éxito");
   } catch (error) {
     console.error("Error general:", error);
     res.status(500).send("Error general: " + error.message);
-    throw new Error("Error general:", error);
   }
 });
 
